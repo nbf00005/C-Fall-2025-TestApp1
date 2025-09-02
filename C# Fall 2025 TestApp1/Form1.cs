a2a9cf76e4b685d15d9edf2420bf373485bd3546
nicho <nicho@NBF_LPTP_FEB25>
2025-09-02 10:01:20 -0400

    Add project files.

---
+++ C# Fall 2025 TestApp1/Form1.cs
@@ -0,0 +1,15 @@
+namespace C__Fall_2025_TestApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HEY! Like, watch it, dude!");
+        }
+    }
+}
